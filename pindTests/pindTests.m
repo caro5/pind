353cb2a1847e5b84ac712e2c9e367f22d5288dd9
Caroline Wong <carowong5@gmail.com>
2014-02-19 20:51:24 -0600

    Initial Commit

---
+++ pindTests/pindTests.m
@@ -0,0 +1,34 @@
+//
+//  pindTests.m
+//  pindTests
+//
+//  Created by Caroline Wong on 2/19/14.
+//  Copyright (c) 2014 Caroline Wong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface pindTests : XCTestCase
+
+@end
+
+@implementation pindTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
